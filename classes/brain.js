18ca8396f236f3e9b6ed2b0baf383829a2940ebc
ben mcleay <ben.mcleay@gmail.com>
2015-12-11 15:04:10 +1000

    Started brain refactor

---
+++ classes/brain.js
@@ -0,0 +1,30 @@
+Brain = function (orb) {
+	this.orb = orb;
+	this.rules = orb.seed.rules;
+}
+
+Brain.prototype.Think = function () {
+	for (var i = 0; i < this.rules.length; i++) {
+		
+		var rule = this.rules[i];
+		
+		if (this.EvaluateRule(rule)) {
+			this.ExecuteInstruction(rule.instruction);
+			continue;
+		}
+		
+	}
+}
+
+Brain.prototype.EvaluateRule = function (rule) {
+	
+	var value = this.orb[rule.attribute];
+	var test = rule.test;
+	var condition = rule.condition;
+	var instruction = rule.instruction;
+	
+}
+
+Brain.prototype.ExecuteInstruction = function (instruction) {
+	
+}
